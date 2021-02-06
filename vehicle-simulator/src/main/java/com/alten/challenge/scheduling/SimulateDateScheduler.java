5610b9dfeddff698445d146a2de65005329ec797
k.yadmand <k.yadmand@kian.digital>
2021-02-07 00:43:21 +0330

    first commit

--- vehicle-simulator/src/main/java/com/alten/challenge/scheduling/SimulateDateScheduler.java
+++ vehicle-simulator/src/main/java/com/alten/challenge/scheduling/SimulateDateScheduler.java
@@ -1,4 +1,35 @@
 package com.alten.challenge.scheduling;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 public class SimulateDateScheduler {
+
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator1(){
+
+    }
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator2(){
+
+    }
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator3(){
+
+    }
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator4(){
+
+    }
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator5(){
+
+    }
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator6(){
+
+    }
+    @Scheduled(fixedRate = 60000)
+    public void vehicleDataGenerator7(){
+
+    }
 }

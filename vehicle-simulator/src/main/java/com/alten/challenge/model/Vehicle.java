4a65107bf70b7f869ea0c5150df68848005e8fc8
k.yadmand <k.yadmand@kian.digital>
2021-02-06 23:15:11 +0330

    first commit

--- vehicle-simulator/src/main/java/com/alten/challenge/model/Vehicle.java
+++ vehicle-simulator/src/main/java/com/alten/challenge/model/Vehicle.java
@@ -1,11 +1,15 @@
 package com.alten.challenge.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Data
 public class Vehicle {
+    @Id
+    private String id;
     private String vin;
     private String regNr;
+    private String customerId;
 }

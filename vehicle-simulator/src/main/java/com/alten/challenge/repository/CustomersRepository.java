10b39e79bb6bb2945fb533187023e32498388d4f
k.yadmand <k.yadmand@kian.digital>
2021-02-06 23:37:17 +0330

    first commit

--- vehicle-simulator/src/main/java/com/alten/challenge/repository/CustomersRepository.java
+++ vehicle-simulator/src/main/java/com/alten/challenge/repository/CustomersRepository.java
@@ -2,6 +2,8 @@ package com.alten.challenge.repository;
 
 import com.alten.challenge.model.Customer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface CustomersRepository extends ReactiveMongoRepository<Customer,String> {
+    Mono<Customer> findByFullName(String fullName);
 }
--- vehicle-simulator/src/main/java/com/alten/challenge/utill/ApplicationStartup.java
+++ vehicle-simulator/src/main/java/com/alten/challenge/utill/ApplicationStartup.java
@@ -24,70 +24,70 @@ public class ApplicationStartup
      */
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-        Customer kallesGrustransporte  = new Customer();
-        kallesGrustransporte.setAddress("Cementvägen 8, 111 11 Södertälje");
-        kallesGrustransporte.setFullName("Kalles Grustransporte");
-        customersRepository.save(kallesGrustransporte).flatMap(customer -> {
-            List<Vehicle> kallesVehicle = new ArrayList<>();
-            Vehicle kv1 = new Vehicle();
-            kv1.setRegNr("ABC123");
-            kv1.setVin("YS2R4X20005399401");
-            kv1.setCustomerId(customer.getId());
-            Vehicle kv2 = new Vehicle();
-            kv2.setRegNr("DEF456");
-            kv2.setVin("VLUR4X20009093588");
-            kv2.setCustomerId(customer.getId());
-            Vehicle kv3 = new Vehicle();
-            kv3.setRegNr("GHI789");
-            kv3.setVin("VLUR4X20009048066");
-            kv3.setCustomerId(customer.getId());
-            kallesVehicle.add(kv1);
-            kallesVehicle.add(kv2);
-            kallesVehicle.add(kv3);
+        if(customersRepository.findByFullName("Kalles Grustransporte").block()==null){
+            Customer kallesGrustransporte  = new Customer();
+            kallesGrustransporte.setAddress("Cementvägen 8, 111 11 Södertälje");
+            kallesGrustransporte.setFullName("Kalles Grustransporte");
+            customersRepository.save(kallesGrustransporte).subscribe(customer -> {
+                List<Vehicle> kallesVehicle = new ArrayList<>();
+                Vehicle kv1 = new Vehicle();
+                kv1.setRegNr("ABC123");
+                kv1.setVin("YS2R4X20005399401");
+                kv1.setCustomerId(customer.getId());
+                Vehicle kv2 = new Vehicle();
+                kv2.setRegNr("DEF456");
+                kv2.setVin("VLUR4X20009093588");
+                kv2.setCustomerId(customer.getId());
+                Vehicle kv3 = new Vehicle();
+                kv3.setRegNr("GHI789");
+                kv3.setVin("VLUR4X20009048066");
+                kv3.setCustomerId(customer.getId());
+                kallesVehicle.add(kv1);
+                kallesVehicle.add(kv2);
+                kallesVehicle.add(kv3);
+                vehiclesRepository.saveAll(kallesVehicle).subscribe();
+            });
 
-        }).subscribe();
-        List<Vehicle> kallesVehicle = new ArrayList<>();
-        Vehicle kv1 = new Vehicle();
-        kv1.setRegNr("ABC123");
-        kv1.setVin("YS2R4X20005399401");
-        Vehicle kv2 = new Vehicle();
-        kv2.setRegNr("DEF456");
-        kv2.setVin("VLUR4X20009093588");
-        Vehicle kv3 = new Vehicle();
-        kv3.setRegNr("GHI789");
-        kv3.setVin("VLUR4X20009048066");
-        kallesVehicle.add(kv1);
-        kallesVehicle.add(kv2);
-        kallesVehicle.add(kv3);
-        /* Johans Bulk*/
-        Customer johans = new Customer();
-        johans.setFullName("Johans Bulk");
-        johans.setAddress("alkvägen 12, 222 22 Stockholm");
-        List<Vehicle> johansVehicle = new ArrayList<>();
-        Vehicle jv1 = new Vehicle();
-        jv1.setRegNr("JKL012");
-        jv1.setVin("YS2R4X20005388011");
-        Vehicle jv2 = new Vehicle();
-        jv2.setRegNr("MNO345");
-        jv2.setVin("YS2R4X20005387949");
-        johansVehicle.add(jv1);
-        johansVehicle.add(jv2);
+        }
+        if(customersRepository.findByFullName("Johans Bulk").block()==null){
+            Customer johans = new Customer();
+            johans.setFullName("Johans Bulk");
+            johans.setAddress("alkvägen 12, 222 22 Stockholm");
+            customersRepository.save(johans).subscribe(customer -> {
+                List<Vehicle> johansVehicle = new ArrayList<>();
+                Vehicle jv1 = new Vehicle();
+                jv1.setRegNr("JKL012");
+                jv1.setVin("YS2R4X20005388011");
+                jv1.setCustomerId(customer.getId());
+                Vehicle jv2 = new Vehicle();
+                jv2.setRegNr("MNO345");
+                jv2.setVin("YS2R4X20005387949");
+                jv2.setCustomerId(customer.getId());
+                johansVehicle.add(jv1);
+                johansVehicle.add(jv2);
+                vehiclesRepository.saveAll(johansVehicle).subscribe();
+            });
+        }
+        if(customersRepository.findByFullName("Johans Bulk").block()==null){
+            Customer haralds = new Customer();
+            haralds.setAddress("Budgetvägen 1, 333 33 Uppsala");
+            haralds.setFullName("Haralds Värdetransporter");
+            customersRepository.save(haralds).subscribe(customer -> {
+                List<Vehicle> haraldsVehicle = new ArrayList<>();
+                Vehicle hv1 = new Vehicle();
+                hv1.setRegNr("PQR678");
+                hv1.setVin("YS2R4X20005387765");
+                hv1.setCustomerId(customer.getId());
+                Vehicle hv2 = new Vehicle();
+                hv2.setRegNr("STU901");
+                hv2.setVin("YS2R4X20005387055");
+                hv2.setCustomerId(customer.getId());
+                haraldsVehicle.add(hv1);
+                haraldsVehicle.add(hv2);
+                vehiclesRepository.saveAll(haraldsVehicle).subscribe();
 
-        /*Haralds Värdetransporter*/
-        Customer haralds = new Customer();
-        haralds.setAddress("Budgetvägen 1, 333 33 Uppsala");
-        haralds.setFullName("Haralds Värdetransporter");
-
-        List<Vehicle> haraldsVehicle = new ArrayList<>();
-        Vehicle hv1 = new Vehicle();
-        hv1.setRegNr("PQR678");
-        hv1.setVin("YS2R4X20005387765");
-        Vehicle hv2 = new Vehicle();
-        hv2.setRegNr("STU901");
-        hv2.setVin("YS2R4X20005387055");
-        johansVehicle.add(jv1);
-        johansVehicle.add(jv2);
-
+            });
+        }
         return;
     }
 }
